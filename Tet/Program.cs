b4dde0373fcd831166e38f957b53cdcf807d26c6
Максим <bashermorningmax@gmail.com>
2021-12-13 18:04:04 +0300

    Добавьте файлы проекта.

---
+++ Tet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistryExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryExplorer re = new RegistryExplorer();
+            re.PrintRegistry();
+        }
+    }
+}
+
+
---
+++ Tet/RegistryExplorer.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace RegistryExporter
+{
+    public class RegistryExplorer
+    {
+        private string _registryPath;
+        private string _SID = System.Security.Principal.WindowsIdentity.GetCurrent().User.Value;
+        public RegistryExplorer()
+        {
+            if(Environment.Is64BitOperatingSystem)
+            {
+                _registryPath = @"SOFTWARE\Wow6432Node\Crypto Pro\Settings\USERS\" + _SID + @"\Keys";
+            }
+            else
+            {
+                _registryPath = @"SOFTWARE\Crypto Pro\Settings\USERS\" + _SID + @"\Keys";
+            }
+        }
+      
+        public void PrintRegistry()
+        {          
+            RegistryKey rk = Registry.LocalMachine;
+            rk = rk.OpenSubKey(_registryPath);
+            PrintKeys(rk);
+        }
+
+        private void PrintKeys(RegistryKey rkey)
+        {
+            string[] keyNames = rkey.GetSubKeyNames();
+
+            rkey = rkey.OpenSubKey(keyNames[0]);
+            string[] keyFields =
+            {
+                "header.key",
+                "masks.key",
+                "masks2.key",
+                "name.key",
+                "primary.key",
+                "primary2.key"
+            };
+
+            object keyHex = rkey.GetValue("header.key");
+
+            byte[] barr = (byte[])keyHex;
+            foreach (var item in barr)
+            {
+                string tempHex = Convert.ToString(item, 16);
+                tempHex = AddZeroBeforeSingleSymbol(tempHex);
+                Console.WriteLine(tempHex);
+            }
+        }
+
+        private string AddZeroBeforeSingleSymbol(string letter)
+        {
+            string hexNumb = letter.ToLower();
+            if (hexNumb.Length == 1)
+            {
+                hexNumb = "0" + hexNumb;
+            }
+            return hexNumb;
+        }
+    }
+}
+
+
